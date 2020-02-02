78ec61cc87a6a6cbb583c3e8cf3b644313e78488
tristinxie <53798468+tristinxie@users.noreply.github.com>
2020-02-01 21:15:14 -0800

    some progress

--- public/app.js
+++ public/app.js
@@ -1,8 +1,34 @@
+//import firebase from "firebase/app";
+//require('firebase/firestore')
+//require('firebase/auth')
+//var firebaseConfig = {
+  //apiKey: "AIzaSyCyoYFbIY_-vM_j8zBSmyvTSauBXo10lc8",
+  //authDomain: "lawyermatch.firebaseapp.com",
+  //databaseURL: "https://lawyermatch.firebaseio.com",
+  //projectId: "lawyermatch",
+  //storageBucket: "lawyermatch.appspot.com",
+  //messagingSenderId: "sender-id",
+  //appId: "1:751073412439:web:9a8d7e94d891ecd929ea12",
+//};
+//firebase.initializeApp(firebaseConfig);
 document.addEventListener("DOMContentLoaded", event => {
+    console.log("hello");
     const app = firebase.app();
-    console.log(app)
+    
+    const db = firebase.firestore();
+
+    const myOrg = db.collection('organizations').doc('firstorg');
+
+    myOrg.onSnapshot(doc => {
+        const data = doc.data();
+        document.getElementById('a').innerHTML = "org is: " + data.name
+    })
+
 });
 
+function showDetainee(){
+
+}
 function googleLogin() {
     const provider = new firebase.auth.GoogleAuthProvider();
     
@@ -10,8 +36,29 @@ function googleLogin() {
         
             .then(result => {
                 const user = result.user;
-                document.write(`Hello ${user.displayName}`)
-                console.log(user)        
+                document.write(`Hello ${user.displayName}`)       
             }) 
             .catch(console.log) 
+}
+function emailLogin(){
+    var email = app.email;
+    var password = app.password;
+
+    if (!email || !password){
+        return console.log('Please enter your email and password')
+    }
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log('sign in error', error)
+        // ...
+
+      });
+}
+
+function updateOrg(e){
+    const db = firebase.firestore();
+    const myOrg = db.collection('organizations').doc('firstorg');
+    myOrg.update({name : e.target.value});
 }
