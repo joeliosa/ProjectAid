3c1fe0a0517c01f49e2b8c7bb7aafaa924487eae
Joel Saarinen <johowin@gmail.com>
2020-02-01 16:02:18 -0800

    added code from tutorial page

---
+++ index.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCyoYFbIY_-vM_j8zBSmyvTSauBXo10lc8",
+  authDomain: "lawyermatch.firebaseapp.com",
+  databaseURL: "https://lawyermatch.firebaseio.com",
+  projectId: "lawyermatch",
+  storageBucket: "lawyermatch.appspot.com",
+  messagingSenderId: "sender-id",
+  appId: "1:751073412439:web:9a8d7e94d891ecd929ea12",
+};
